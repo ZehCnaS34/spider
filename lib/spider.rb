31a5e3cf77827d1371f2505a5b4d080916ce3668
alexander sanchez <asanchez.dev@gmail.com>
2014-12-05 03:31:22 -0500

    asdfoij

--- lib/spider.rb
+++ lib/spider.rb
@@ -6,56 +6,31 @@ require 'json'
 
 
 module Spider
-  attr_reader :links
-  # Your code goes here...
-
-
-  def get_response(uri)
-    if uri =~ /http:\/\//
-       Net::HTTP.get_response(URI.parse(uri))
-    else
-      Net::HTTP.get_response(URI.parse("http://#{uri}"))
-    end
-
-  end
 
 
   def initialize
-    @db = Redis.new(port: 6379)
+    # @db = Redis.new(port: 6379)
     @links = Array.new
   end
 
   def crawl(depth=10)
-    @links = JSON.parse(@db.get('links'))
-    at = 0
     @links.each do |l|
-      if at == depth
-        break
-      end
+      break if depth == 0
       seed(l)
-      at += 1
+      at -= 1
     end
-    puts @links
-    puts @links.length
-    @db.set "links", @links.to_json
   end
-
-
-  def fetch_page(uri)
-    @body = get_response(uri)
-  end
-
 
   def scrape
-    return nil if @body.nil
-
-    @links.concat get_response(uri).body.split("<")
-      .map(&has_dot_com).join.split(/href=['"]/)
-      .map(&has_href)
-      .map(&nil_or_contains_url)
-      .map(&has_id)
-      .compact.uniq
-    puts @links.count
+    return nil if @body.nil?
+    @links.concat @body.split(/href=['"]/)
+    puts @links
+    # return self for method chaining
     self
+  end
+
+  def seed(location)
+    @body = get_response(location).body
+    scrape
   end
 end
--- lib/spider/util.rb
+++ lib/spider/util.rb
@@ -1,21 +1,42 @@
-module Util
-  def nil_or_contains_url(e)
-    e if  e =~ /http:\/\/.+(.com|.net|.org|edu)/
-  end
+require 'net/http'
+require 'json'
 
-  def has_dot_com(i)
-    i.split(/['"]/)[0]
-  end
+module Spider
+  module Util
+    def nil_or_contains_url(e)
+      puts "fuck"
+      puts e
+      e if  e =~ /http:\/\/.+(.com|.net|.org|edu)/
+    end
 
-  def has_href(i)
-    i if i =~ /href=/
-  end
+    def has_dot_com(i)
+      i.split(/['"]/)[0]
+    end
 
-  def has_quote(i)
-    i.split(/['"]/)[0]
-  end
+    def has_href(i)
+      i if i =~ /href=/
+    end
 
-  def has_id(i)
-  end
+    def has_quote(i)
+      i.split(/['"]/)[0]
+    end
 
+    def has_id(i)
+    end
+
+    def get_response(uri)
+      if uri =~ /http:\/\//
+        Net::HTTP.get_response(URI.parse(uri))
+      else
+        Net::HTTP.get_response(URI.parse("http://#{uri}"))
+      end
+
+    end
+
+    def fetch_page(uri)
+      @body = get_response(uri)
+    end
+
+
+  end
 end

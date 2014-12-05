d55a2fe1806b32c8b19aad0f245c4d8fca30089c
alexander sanchez <asanchez.dev@gmail.com>
2014-12-04 20:21:52 -0500

    fixing a bit

--- lib/spider.rb
+++ lib/spider.rb
@@ -2,31 +2,33 @@ require "spider/version"
 require 'net/http'
 
 module Spider
+  attr_reader :links
   # Your code goes here...
-
-  def look
-    uri = "http://stackoverflow.com/questions/1878891/how-to-load-a-web-page-and-search-for-a-word-in-ruby"
-    response = Net::HTTP.get_response(URI.parse(uri))
+  def initialize
     @links = Array.new
+  end
 
+
+  def seed(uri)
+    response = Net::HTTP.get_response(URI.parse(uri))
 
     ## start parsing the reponse body
     response.body.split("\n").each do |e|
-
-
       ## if href is matched
       if e =~ /href=/
         meta_link = /href=['"].+['"]/.match(e).to_s
+        # remove the href from the meta_link
         meta_link = meta_link[6..meta_link.length]
-        puts "new meta"
-        puts meta_link
+
+        # define where the href locations stops
         stopper = meta_link.to_s.index(/['"]/)
         if meta_link.to_s.length > 1
-          @links << meta_link.to_s[0..stopper]
+          @links << meta_link.to_s[0..stopper-1]
         end
-
       end
     end
+
+    @links = @links.map { |l| l if l =~ /http:\/\// }.compact
 
     puts @links
   end

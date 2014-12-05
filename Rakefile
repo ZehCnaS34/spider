fa1f490ca0fe7ebb42fc81e14c2e5f9e469aaaa8
alexander sanchez <asanchez.dev@gmail.com>
2014-12-04 22:41:06 -0500

    changed the look algorithm

--- Rakefile
+++ Rakefile
@@ -1,6 +1,8 @@
 require "bundler/gem_tasks"
 
 task :default do
+  puts "building"
   `gem build spider.gemspec`
+  puts "installing"
   `gem install spider-*.gem`
 end
--- lib/spider.rb
+++ lib/spider.rb
@@ -5,11 +5,16 @@ module Spider
   attr_reader :links
   # Your code goes here...
 
+  def nil_or_contains_url(e)
+    e if  e =~ /http:\/\/.+(.com|.net|.org|edu)/
+  end
+
+
   def get_response(uri)
     if uri =~ /http:\/\//
-      response = Net::HTTP.get_response(URI.parse(uri))
+       Net::HTTP.get_response(URI.parse(uri))
     else
-      response = Net::HTTP.get_response(URI.parse("http://#{uri}"))
+      Net::HTTP.get_response(URI.parse("http://#{uri}"))
     end
 
   end
@@ -28,50 +33,71 @@ module Spider
       at += 1
     end
     puts @links
+    puts @links.length
   end
 
   def seed(uri)
-    response = get_response(uri)
+
+    @links.concat get_response(uri).body.split("<").map{|i|
+      i if i =~ /(.com|.net)/
+    }.compact.join.split(/href=['"]/).map{ |i|
+      i.split(/['"]/)[0]
+    }.compact.map{|i|
+      nil_or_contains_url i
+    }.compact.uniq
+
+    # @links = get_response(uri)).body.split("<").map{ |i|
+  #   i if i =~ /(.com|.net)/
+  # }.compact.join.split(/href=['"]/).map{ |i|
+  #   i.split(/['"]/)[0] }.map{ |i|
+  #   nil_or_contains_url i
+  # }.compact.uniq
 
 
-    ## start parsing the reponse body
-    response.body.split("\n").each do |e|
-      ## if href is matched
-      if e =~ /href=/
-        meta_link = /href=['"].+['"]/.match(e).to_s
-        # remove the href from the meta_link
-        meta_link = meta_link[6..meta_link.length]
-
-        # define where the href locations stops
-        stopper = meta_link.to_s.index(/['"]/)
-        if meta_link.to_s.length > 1
-          # almost in final form
-          meta_link = meta_link.to_s[0..stopper-1]
-
-          ## if link starts with http
-          if meta_link =~ /http:\/\//
-
-            meta_link = meta_link[7..meta_link.length]
-            loc_index = meta_link.index("/")
-            # puts meta_link
-            # puts loc_index
+    # response.body.split(/['"]/).map{ |i|
+    #   nil_or_contains_url i
+    # }.compact.uniq.each do |f|
+    #   @links << f
+    # end
 
 
-            if loc_index
-              @links << meta_link[0..loc_index-1] if @links.index(meta_link[0..loc_index-1]).nil?
-            else
-              @links << meta_link if @links.index(meta_link).nil?
-            end
+    # ## start parsing the reponse body
+    # response.body.split("<").each do |e|
+    #   ## if href is matched
+    #   if e =~ /href=/
+    #     meta_link = /href=['"].+['"]/.match(e).to_s
+    #     # remove the href from the meta_link
+    #     meta_link = meta_link[6..meta_link.length]
 
-          end
+    #     # define where the href locations stops
+    #     stopper = meta_link.to_s.index(/['"]/)
+    #     if meta_link.to_s.length > 1
+    #       # almost in final form
+    #       meta_link = meta_link.to_s[0..stopper-1]
+
+    #       ## if link starts with http
+    #       if meta_link =~ /http:\/\//
+
+    #         meta_link = meta_link[7..meta_link.length]
+    #         loc_index = meta_link.index("/")
+    #         # puts meta_link
+    #         # puts loc_index
+
+    #         if loc_index
+    #           @links << meta_link[0..loc_index-1] if @links.index(meta_link[0..loc_index-1]).nil?
+    #         else
+    #           @links << meta_link if @links.index(meta_link).nil?
+    #         end
+
+    #       end
 
 
-        end
-      end
-    end
+    #     end
+    #   end
+    # end
 
     # @links = @links.map { |l| l if l =~ /http:\/\// }.compact.uni
-    @links = @links.compact.uniq
+
     puts @links.count
     self
   end
